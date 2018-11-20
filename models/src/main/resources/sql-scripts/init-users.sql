052e2d2a2f32f82706de86116b01c5287df98f03
David <david.senica95@gmail.com>
2018-11-20 15:56:33 +0100

    Added cloud database

--- models/src/main/resources/sql-scripts/init-users.sql
+++ models/src/main/resources/sql-scripts/init-users.sql
@@ -1,3 +1,3 @@
-INSERT INTO my_user ( first_name, last_name, email, password, date_of_birth) VALUES ('James', 'Smith', 'james.smith@gmail.com', 'test', TIMESTAMP '2006-01-01 15:36:38');
-INSERT INTO my_user ( first_name, last_name, email, password, date_of_birth) VALUES ('John', 'Williams', 'john.williams@gmail.com', 'test', TIMESTAMP '2009-03-17 15:36:38');
-INSERT INTO my_user ( first_name, last_name, email, password, date_of_birth) VALUES ('Robert', 'Jones', 'robert.jones@gmail.com', 'test', TIMESTAMP '2013-11-29 15:36:38');
+--INSERT INTO my_user ( first_name, last_name, email, password, date_of_birth) VALUES ('James', 'Smith', 'james.smith@gmail.com', 'test', TIMESTAMP '2006-01-01 15:36:38');
+--INSERT INTO my_user ( first_name, last_name, email, password, date_of_birth) VALUES ('John', 'Williams', 'john.williams@gmail.com', 'test', TIMESTAMP '2009-03-17 15:36:38');
+--INSERT INTO my_user ( first_name, last_name, email, password, date_of_birth) VALUES ('Robert', 'Jones', 'robert.jones@gmail.com', 'test', TIMESTAMP '2013-11-29 15:36:38');
